715b78aa8677832393ab20a69c96173e8779481e
pacosta1982 <acosta.aleoli@gmail.com>
2019-07-15 10:00:07 -0400

    ajustes

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,6 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
-<li><a href="{{ backpack_url('elfinder') }}"><i class="fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
+
 
 <!-- Users, Roles Permissions -->
   <li class="treeview">
@@ -12,7 +12,7 @@
     </ul>
   </li>
 
-<li><a href="{{ backpack_url('elfinder') }}"><i class="fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
+
 <li><a href='{{ backpack_url('categorias') }}'><i class='fa fa-tag'></i> <span>Categorias</span></a></li>
 <li><a href='{{ backpack_url('rubros') }}'><i class='fa fa-tag'></i> <span>Rubros</span></a></li>
 <li><a href='{{ backpack_url('units') }}'><i class='fa fa-tag'></i> <span>Unidades</span></a></li>
